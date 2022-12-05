af034bbcc91ed912fde1f446f6cfb7d06b39fc57
MGen30 <gen30.max@gmail.com>
2022-12-05 14:34:44 +0100

    wip

--- LF08-LogFileProject/LF08-LogFileProject/Form1.Designer.cs
+++ LF08-LogFileProject/LF08-LogFileProject/Form1.Designer.cs
@@ -31,40 +31,116 @@ partial class Form1
     {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listLogsControl1 = new LF08_LogFileProject.ListLogsControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 193);
+            this.dataGridView1.Location = new System.Drawing.Point(26, 247);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(263, 188);
+            this.dataGridView1.Size = new System.Drawing.Size(705, 535);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(609, 116);
+            this.button1.Location = new System.Drawing.Point(561, 798);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.Size = new System.Drawing.Size(170, 34);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Import Log Daten";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 33);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Analyse Methode:";
+            // 
+            // listLogsControl1
+            // 
+            this.listLogsControl1.Location = new System.Drawing.Point(26, 82);
+            this.listLogsControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.listLogsControl1.Name = "listLogsControl1";
+            this.listLogsControl1.Size = new System.Drawing.Size(1202, 162);
+            this.listLogsControl1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(420, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Listet alle Log-Einträge innerhalb der Datenbank auf";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(308, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Beschreibung";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(784, 483);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(260, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hier kommen die Log-Infos hin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(266, 483);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Hier stehen die Logs";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1258, 844);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listLogsControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Log-File-Analyser von Lina, Lisa und Max";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -72,4 +148,11 @@ partial class Form1
 
     private DataGridView dataGridView1;
     private Button button1;
+    private ComboBox comboBox1;
+    private Label label1;
+    private ListLogsControl listLogsControl1;
+    private Label label2;
+    private Label label3;
+    private Label label4;
+    private Label label5;
 }
---
+++ LF08-LogFileProject/LF08-LogFileProject/ListLogsControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LF08_LogFileProject
+{
+    public partial class ListLogsControl : UserControl
+    {
+        public ListLogsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
